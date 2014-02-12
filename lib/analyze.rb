d4e12754ea71ade8d3e283c1ea521f3ab1ca1d1e
twurz <travis.wurzer@yahoo.com>
2014-02-11 20:39:19 -0800

    second regular expression added.

--- lib/analyze.rb
+++ lib/analyze.rb
@@ -1,1 +1,34 @@
 # put your solution to code challenge 02 here
+class PrefixSuffix
+  attr_reader :regex_hash
+
+  def initialize
+    @access = ARGV[0]
+    @raw_customers = STDIN
+    @regex_hash = Hash.new
+  end
+
+  create `histogram.txt`
+  
+  def sorter
+    case @access
+    when '-p'
+      @raw_customers.each_line { |line| prefix_regex(line) } 
+    when '-s'
+      @raw_customers.each_line { |line| suffix_regex(line) }
+    else
+      puts "unknown option"
+      puts "usage: analyze.rb -p | -s < input_file > output_file"
+    end
+  end 
+
+  def prefix_regex(line)
+    prefix = /(^\w+[\.]*)/.match(line).to_s
+    
+  end
+
+  def suffix_regex(line)
+    suffix = /(?!x)([a-z]+[\.]*)/.match(line)
+    suffix.flatten!
+  end
+end
